f6743f76d4b4edea68d368aed7db462e0a3bb37c
0x27sir42 <fizzanetworkai@gmail.com>
2026-01-10 04:59:44 +0700

    Create price.js

---
+++ js/price.js
@@ -0,0 +1,11 @@
+new TradingView.widget({
+  width: "100%",
+  height: 400,
+  symbol: "POLUSDT",
+  interval: "15",
+  timezone: "Etc/UTC",
+  theme: "dark",
+  style: "1",
+  locale: "en",
+  container_id: "tv_chart"
+});
