0cbca944bc6863d30bd9f7ccc921cbbbd0a97461
0x27sir42 <fizzanetworkai@gmail.com>
2026-01-10 05:14:07 +0700

    Update wallet.js

--- js/wallet.js
+++ js/wallet.js
@@ -1,16 +1,9 @@
-let userAccount = null;
-
 async function connectWallet() {
   if (!window.ethereum) {
     alert("Please install MetaMask");
     return;
   }
-
-  const accounts = await ethereum.request({
-    method: "eth_requestAccounts"
-  });
-
-  userAccount = accounts[0];
+  const acc = await ethereum.request({ method: "eth_requestAccounts" });
   document.getElementById("walletAddress").innerText =
-    "Wallet: " + userAccount;
+    "Wallet: " + acc[0];
 }
