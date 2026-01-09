fe38cfb3b1c27607ac2a6b4dd63b5d8ad91414a5
0x27sir42 <fizzanetworkai@gmail.com>
2026-01-10 05:30:43 +0700

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,4 @@
-const images = [
+let ecoImages = [
   "assets/img/eco1.jpg",
   "assets/img/eco2.jpg",
   "assets/img/eco3.jpg",
@@ -6,22 +6,13 @@ const images = [
 ];
 
 let index = 0;
-const img = document.getElementById("ecoImage");
 
 function nextEco() {
-  index = (index + 1) % images.length;
-  rotate();
+  index = (index + 1) % ecoImages.length;
+  document.getElementById("ecoImage").src = ecoImages[index];
 }
 
 function prevEco() {
-  index = (index - 1 + images.length) % images.length;
-  rotate();
-}
-
-function rotate() {
-  img.style.transform = "rotateY(90deg)";
-  setTimeout(() => {
-    img.src = images[index];
-    img.style.transform = "rotateY(0deg)";
-  }, 300);
+  index = (index - 1 + ecoImages.length) % ecoImages.length;
+  document.getElementById("ecoImage").src = ecoImages[index];
 }
