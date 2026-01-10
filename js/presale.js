e7d8d7a596e839299a74e99f9b3f7df88d0b2c8e
0x27sir42 <fizzanetworkai@gmail.com>
2026-01-10 10:08:45 +0700

    Update presale.js

--- js/presale.js
+++ js/presale.js
@@ -1,47 +1,16 @@
-const PRESALE_ADDRESS = "0x24CD5fc39Fa409C75f14eB9f2FE25f9cbb324Ffb";
 const RATE = 1560;
-const MIN_BUY = 10; // 10 POL minimum
-
-const PRESALE_ABI = [
-  "function buy() payable",
-  "function claim()"
-];
-
-let presale;
+const MIN_POL = 10;
 
 function calcZila() {
-  const pol = Number(document.getElementById("polAmount").value || 0);
+  const pol = Number(document.getElementById("polAmount").value);
   document.getElementById("zilaAmount").innerText = pol * RATE;
 }
 
-async function initPresale() {
-  if (!signer) {
-    alert("Please connect wallet first");
-    return;
-  }
-  presale = new ethers.Contract(PRESALE_ADDRESS, PRESALE_ABI, signer);
-}
-
 async function buyZila() {
-  await initPresale();
+  if (!userAddress) return alert("Connect wallet first");
 
   const pol = Number(document.getElementById("polAmount").value);
-  if (!pol || pol < MIN_BUY) {
-    alert("Minimum purchase is 10 POL");
-    return;
-  }
+  if (pol < MIN_POL) return alert("Minimum buy is 10 POL");
 
-  const tx = await presale.buy({
-    value: ethers.utils.parseEther(pol.toString())
-  });
-
-  await tx.wait();
-  alert("Presale purchase successful");
-}
-
-async function claimPresale() {
-  await initPresale();
-  const tx = await presale.claim();
-  await tx.wait();
-  alert("Presale claimed");
+  alert("Presale tx handled by smart contract");
 }
